690e079be8a2f7220d58e5d13b5731524e19ca78
ibianche <inna.pomorska@gmail.com>
2022-08-09 14:24:21 +0200

    rozdzial 6 linter, debugger

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { sum } from "./calc";
 let printMessage = (msg: string): void => console.log(`Komunikat: ${ msg }`);
 let message = ("Witaj, TypeScript");
 printMessage(message);
-debugger;
+debugger; // eslint-disable-line no-debugger
 
 let total = sum(100, 200, 300);
 console.log(`Wartość całkowita: ${total}`); 
