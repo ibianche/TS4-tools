a0c5e28927a5593e69daf33fe9cf59ff27f7cf27
ibianche <inna.pomorska@gmail.com>
2022-08-09 12:46:05 +0200

    rodz 6

--- dist/index.js
+++ dist/index.js
@@ -4,5 +4,7 @@ const calc_1 = require("./calc");
 let printMessage = (msg) => console.log(`Komunikat: ${msg}`);
 let message = ("Witaj, TypeScript");
 printMessage(message);
+debugger;
 let total = calc_1.sum(100, 200, 300);
 console.log(`Wartość całkowita: ${total}`);
+//# sourceMappingURL=index.js.map
